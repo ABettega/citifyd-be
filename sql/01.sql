2fe1a5eb5befb09a7793884baa909a6d8d50e803
Aroldo Bettega <aroldo.bettega@gmail.com>
2022-12-05 00:27:13 -0300

    Improvement: Database changes

--- sql/01.sql
+++ sql/01.sql
@@ -1,12 +1,16 @@
 INSERT INTO store
     (store_name, store_fee)
 VALUES
-    ('Store 1', 0.1),
-    ('Store 2', 0.05),
-    ('Store 3', 0);
+    ('Store 1', 10),
+    ('Store 2', 5),
+    ('Store 3', 1);
 
 INSERT INTO product
-    (product_name, product_value, product_active)
+    (product_name, product_value, product_active, store_id)
 VALUES
-    ('Toothpaste', 5, TRUE),
-    ('Toothbrush', 15, TRUE);
+    ('Toothpaste', 5, TRUE, 1),
+    ('Toothpaste', 5, TRUE, 2),
+    ('Toothpaste', 5, TRUE, 3),
+    ('Toothbrush', 15, TRUE, 1),
+    ('Toothbrush', 15, TRUE, 2),
+    ('Toothbrush', 15, TRUE, 3);
